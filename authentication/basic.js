bb541e8265bb4553145abab5ea3424f65b7d938d
lowkeyCodee <rizskplay@gmail.com>
2026-04-26 10:33:26 +0800

    authentication

---
+++ authentication/basic.js
@@ -0,0 +1,41 @@
+// Basic Auth;
+
+// app.get('/admin', (req, res) => {
+//     const auth = req.headers.authorization;
+//     if (auth === 'Basic ' + Buffer.from('user:pass').toString('base64')) {
+//         res.send("Welcome, Admin.");
+//     } else {
+//         res.set('WWW-Authenticate', 'Basic');
+//         res.status(401).send("Authentication required.");
+//     }
+// });
+
+// console.log(Buffer.from('user:pass').toString('base64'))
+
+// Session Based
+
+// const session = require('express-session');
+// app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: true }));
+
+// app.post('/login', (req, res) => {
+//     req.session.user = { id: 1, name: 'Alice' }; // Saved on server
+//     res.send("Logged in!");
+// });
+
+
+
+
+// JWT 
+
+
+const jwt = require('jsonwebtoken'); // jwt package
+
+const token = jwt.sign({ userId: 123 }, 'secret_key', { expiresIn: '1h' });
+
+// Middleware to verify
+const auth = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, 'secret_key');
+    req.userId = decoded.userId;
+    next();
+};
